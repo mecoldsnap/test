e2e131da240f2d75101899a754eca85181b74fad
unknown <mecoldsnap@gmail.com>
2024-12-09 23:33:11 +0800

    test commit

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lesson 1</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="device=width-device, initial-scale=1.0">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+
+
+
+        <?php 
+        $number = 1+2;
+        $float = 5.25 + $number;
+        
+        echo "Hello World", $float;?>
+
+    </body>
+</html>
